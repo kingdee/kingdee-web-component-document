7080004e179f48779ba41805c151e339f195b32f
zcy97 <1050365185@qq.com>
2026-02-04 17:12:12 +0800

    commit

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,15 @@ registerIconLibrary('system', {
   resolver: (name: string) =>
     `../node_modules/@kdcloudjs/shoelace/dist/assets/icons/${name}.svg`, // `/path/to/custom/icons/${name}.svg`
 });
-export { default as Button } from './Button'
-export { default as DataTable } from './DataTable'
-export { default as Input } from './Input'
-export { default as InputNumber } from './InputNumber'
-export { default as Tabset } from './Tabset'
-export { default as Tree } from './Tree'
-export { default as Switch } from './Switch'
-export { default as Tag } from './Tag'
-export { default as Card } from './Card'
-export { default as Layout } from './Layout'
-export { default as Spin } from './Spin'
-export { default as Modal } from './Modal';
+// export { default as Button } from './Button'
+// export { default as DataTable } from './DataTable'
+// export { default as Input } from './Input'
+// export { default as InputNumber } from './InputNumber'
+// export { default as Tabset } from './Tabset'
+// export { default as Tree } from './Tree'
+// export { default as Switch } from './Switch'
+// export { default as Tag } from './Tag'
+// export { default as Card } from './Card'
+// export { default as Layout } from './Layout'
+// export { default as Spin } from './Spin'
+// export { default as Modal } from './Modal';
